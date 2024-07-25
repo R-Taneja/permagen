defccf733466b573a78cac951e97a287d8dfbf89
Rohan Taneja <47066511+R-Taneja@users.noreply.github.com>
2024-07-24 20:24:48 -0700

    Error handling

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ async function getFirebaseConfig(runWithConfigFlag = false) {
         if (configExistsAndIsValid) {
             const shouldEditConfig = await p.confirm({ message: 'Existing Firebase configuration found. Do you want to edit it?' });
             if (!shouldEditConfig) {
-                p.outro('Using existing Firebase configuration.');
+                p.outro('Using existing Firebase configuration.')
                 return configFileJSON;
             }
         }
@@ -44,7 +44,7 @@ async function getFirebaseConfig(runWithConfigFlag = false) {
     }
 
     p.intro('\n---------------------Firebase Configuration---------------------');
-    const readyToProceed = await p.confirm({ message: 'Recommended setup:\n  1. Create a new Firebase project\n  2. Enable Storage in production mode\n  3. Change the security rule to "allow read, write: if true;"\n  4. Get your credentials by going through the web app registration process and copying the "firebaseConfig" object\n\nCredentials will only be stored locally on your computer at ~/.permagenConfig.json.\n\nAre you ready to enter your Firebase configuration details?' });
+    const readyToProceed = await p.confirm({ message: 'Recommended setup:\n  1. Create a new Firebase project\n  2. Enable Storage in production mode\n  3. Change the security rule to "allow read, write: if true;"\n  4. Get your credentials by going through the web app registration process and copying the "firebaseConfig" object (also in Project Settings > Web apps > npm)\n\nCredentials will only be stored locally on your computer at ~/.permagenConfig.json.\n\nAre you ready to enter your Firebase configuration details?' });
     if (!readyToProceed) process.exit(0);
 
     const configString = await p.text({ message: 'Paste your Firebase configuration object here:' });
@@ -90,62 +90,66 @@ async function uploadFile(filePath, storage) {
 }
 
 async function main() {
-    const args = process.argv.slice(2);
-    if (args.length === 0) {
-        p.intro(`\n${color.bgMagenta(color.black('-------------------------------------------'))}\n\nWelcome! Permagen generates permalinks for files by storing them in your configured Firebase Storage bucket\n  •To instantly generate a permalink, use this command: npx permagen [local path to file]\n  •If you need to update your Firebase credentials, use the --config flag\n  •To automatically copy permalinks to your clipboard, use the -c flag\n\n${color.bgMagenta(color.black('-------------------------------------------'))}`);
-        const config = await getFirebaseConfig();
-        const app = initializeApp(config);
-        const storage = getStorage(app);
-        
-        const filePath = await p.text({ message: 'Enter the local path to your file:' });
-        if (!filePath) {
-            p.outro('Firebase configured successfully.');
-            process.exit(0);
-        }
+    try {
+        const args = process.argv.slice(2);
+        if (args.length === 0) {
+            p.intro(`\n${color.bgMagenta(color.black('-------------------------------------------'))}\n\nWelcome! Permagen generates permalinks for files by storing them in your configured Firebase Storage bucket\n  •To instantly generate a permalink, use this command: npx permagen [local path to file]\n  •If you need to update your Firebase credentials, use the --config flag\n  •To automatically copy permalinks to your clipboard, use the -c flag\n\n${color.bgMagenta(color.black('-------------------------------------------'))}`);
+            const config = await getFirebaseConfig();
+            const app = initializeApp(config);
+            const storage = getStorage(app);
+            
+            const filePath = await p.text({ message: 'Enter the local path to your file:' });
+            if (!filePath) {
+                p.outro('Firebase configured successfully.');
+                process.exit(0);
+            }
 
-        const filePathFormatted = filePath.replace(/^['"]|['"]$/g, '');
-        if (!fs.existsSync(filePathFormatted)) {
+            const filePathFormatted = filePath.replace(/^['"]|['"]$/g, '');
+            if (!fs.existsSync(filePathFormatted)) {
+                p.outro('Invalid file path.');
+                process.exit(1);
+            }
+
+            const downloadURL = await uploadFile(filePath, storage);
+
+            if (args.includes('-c') || config.autoCopy) {
+                clipboardy.writeSync(downloadURL);
+                p.outro(`${color.bgBlue(color.black('Your file is copied to the clipboard and hosted at:'))} ${downloadURL}`);
+            } else {
+                p.outro(`${color.bgBlue(color.black('Your file is hosted at:'))} ${downloadURL}`);
+            }
+        } else if (args.includes('--config')) {
+            const config = await getFirebaseConfig(true);
+
+            const shouldAutoCopy = await p.confirm({ 
+                message: 'Do you want to always automatically copy permalinks to the clipboard? (This can be changed later)'
+            });
+
+            config.autoCopy = shouldAutoCopy;
+            fs.writeFileSync(CONFIG_PATH, JSON.stringify(config, null, 2));
+
+            p.outro('Settings updated successfully!');
+        } else if (fs.existsSync(args[0])) {
+            console.log(args[0]);
+            const config = await getFirebaseConfig();
+            const app = initializeApp(config);
+            const storage = getStorage(app);
+            const downloadURL = await uploadFile(args[0], storage);
+
+            if (args.includes('-c') || config.autoCopy) {
+                clipboardy.writeSync(downloadURL);
+                p.outro(`${color.bgBlue(color.black('Your file is copied to the clipboard and hosted at:'))} ${downloadURL}`);
+            } else {
+                p.outro(`${color.bgBlue(color.black('Your file is hosted at:'))} ${downloadURL}`);
+            }
+        } else if (!fs.existsSync(args[0])) {
+            console.log(args[0]);
             p.outro('Invalid file path.');
-            process.exit(1);
-        }
-
-        const downloadURL = await uploadFile(filePath, storage);
-
-        if (args.includes('-c') || config.autoCopy) {
-            clipboardy.writeSync(downloadURL);
-            p.outro(`${color.bgBlue(color.black('Your file is copied to the clipboard and hosted at:'))} ${downloadURL}`);
         } else {
-            p.outro(`${color.bgBlue(color.black('Your file is hosted at:'))} ${downloadURL}`);
+            p.outro('Invalid command. Please run the command without any arguments for the interactive setup or provide a valid file path.');
         }
-    } else if (args.includes('--config')) {
-        const config = await getFirebaseConfig(true);
-
-        const shouldAutoCopy = await p.confirm({ 
-            message: 'Do you want to always automatically copy permalinks to the clipboard? (This can be changed later)'
-        });
-
-        config.autoCopy = shouldAutoCopy;
-        fs.writeFileSync(CONFIG_PATH, JSON.stringify(config, null, 2));
-
-        p.outro('Settings updated successfully!');
-    } else if (fs.existsSync(args[0])) {
-        console.log(args[0]);
-        const config = await getFirebaseConfig();
-        const app = initializeApp(config);
-        const storage = getStorage(app);
-        const downloadURL = await uploadFile(args[0], storage);
-
-        if (args.includes('-c') || config.autoCopy) {
-            clipboardy.writeSync(downloadURL);
-            p.outro(`${color.bgBlue(color.black('Your file is copied to the clipboard and hosted at:'))} ${downloadURL}`);
-        } else {
-            p.outro(`${color.bgBlue(color.black('Your file is hosted at:'))} ${downloadURL}`);
-        }
-    } else if (!fs.existsSync(args[0])) {
-        console.log(args[0]);
-        p.outro('Invalid file path.');
-    } else {
-        p.outro('Invalid command. Please run the command without any arguments for the interactive setup or provide a valid file path.');
+    } catch (error) {
+        process.exit(0);
     }
 }
 
